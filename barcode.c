865a5a3f5284268059e3b58b07c6c48e5c4eccde
Rcc10283 <rc10283@bu.edu>
2026-09-22 01:41:11 +0000

    Implement barcode image generation

--- barcode.c
+++ barcode.c
@@ -1,8 +1,113 @@
 #include "barcode.h"
+
 #include <stdio.h>
+#include <stdlib.h>
+
+
+static const char* encodings[10] = {
+    "1110010", // 0
+    "1100110", // 1
+    "1101100", // 2
+    "1010000", // 3
+    "1011100", // 4
+    "1001110", // 5
+    "1000010", // 6
+    "1000100", // 7
+    "1001000", // 8
+    "1110100"  // 9
+};
+
+
+void fill_column(struct image* img, int x, int white) {
+
+    for (int y = 0; y < img->height; y++) {
+
+        int index = y * img->width + x;
+
+        if (white) {
+            img->pixels[index].r = 255;
+            img->pixels[index].g = 255;
+            img->pixels[index].b = 255;
+        } else {
+            img->pixels[index].r = 0;
+            img->pixels[index].g = 0;
+            img->pixels[index].b = 0;
+        }
+    }
+}
 
 
 struct image* barcode(char* data, int width, int height) {
 
-}
+    struct image* img = malloc(sizeof(struct image));
 
+    img->width = width;
+    img->height = height;
+
+    img->pixels = malloc(width * height * sizeof(struct pixel));
+
+    int x = 0;
+
+    /* Left quiet zone: 9 white columns */
+    for (int i = 0; i < 9; i++) {
+        fill_column(img, x, 1);
+        x++;
+    }
+
+    /* Start pattern: 010 */
+    fill_column(img, x++, 0);
+    fill_column(img, x++, 1);
+    fill_column(img, x++, 0);
+
+    /* Encode first 6 digits */
+    for (int i = 0; i < 6; i++) {
+
+        int digit = data[i] - '0';
+        const char* pattern = encodings[digit];
+
+        for (int j = 0; j < 7; j++) {
+
+            if (pattern[j] == '1') {
+                fill_column(img, x++, 1);
+            } else {
+                fill_column(img, x++, 0);
+            }
+        }
+    }
+
+    /* Middle pattern: 10101 */
+    fill_column(img, x++, 1);
+    fill_column(img, x++, 0);
+    fill_column(img, x++, 1);
+    fill_column(img, x++, 0);
+    fill_column(img, x++, 1);
+
+    /* Encode last 6 digits using inverted pattern */
+    for (int i = 6; i < 12; i++) {
+
+        int digit = data[i] - '0';
+        const char* pattern = encodings[digit];
+
+        for (int j = 0; j < 7; j++) {
+
+            if (pattern[j] == '1') {
+                fill_column(img, x++, 0);
+            } else {
+                fill_column(img, x++, 1);
+            }
+        }
+    }
+
+    /* End pattern: 010 */
+    fill_column(img, x++, 0);
+    fill_column(img, x++, 1);
+    fill_column(img, x++, 0);
+
+    /* Right quiet zone: 9 white columns */
+    for (int i = 0; i < 9; i++) {
+        fill_column(img, x, 1);
+        x++;
+    }
+
+    return img;
+}
--- image_calc.c
+++ image_calc.c
@@ -1,23 +1,31 @@
 #include "barcode.h"
 #include "loader.h"
+
 #include <stdlib.h>
 #include <string.h>
 
 
-int main(int argc, char** argv){
-    // TODO: parse the arguments in argv. 
-    // You can expect argv[1] to be the digits to encode
-    // You can expect argv[2] to be the integer width
-    // You can expect argv[3] to be the integer height
-    // You can expect argv[4] to be the output filepath.
+int main(int argc, char** argv) {
 
-    if(argc != 5) {
+    if (argc != 5) {
         printf("Incorrect number of arguments. Expected: ./build/image_calc <barcode_value> <width> <height> <output_image_path>\n");
         return -1;
     }
 
+    char *data = argv[1];
+    int width = atoi(argv[2]);
+    int height = atoi(argv[3]);
+    char *output_filepath = argv[4];
 
-    // TODO: call barcode
-    
-    // TODO: save result to file
+    /* Generate barcode image */
+    struct image* img = barcode(data, width, height);
+
+    /* Save barcode image */
+    saveimage(output_filepath, img);
+
+    /* Free allocated memory */
+    free(img->pixels);
+    free(img);
+
+    return 0;
 }
